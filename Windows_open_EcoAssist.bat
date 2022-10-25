a2eec421084f2eb0232d3dab4d178feda8478274
Peter van Lunteren <85185478+PetervanLunteren@users.noreply.github.com>
2022-10-25 11:02:41 +1000

    added admin check again

--- Windows_open_EcoAssist.bat
+++ Windows_open_EcoAssist.bat
@@ -5,6 +5,28 @@
 echo off
 @setlocal EnableDelayedExpansion
 
+@REM # set admin rights if not already in use (thanks user399109)
+@REM check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+@REM if error flag set, we do not have admin
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 0 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
 @REM # set path voor ecoassist root dir and add to PATH
 set LOCATION_ECOASSIST_FILES=%HOMEPATH%\EcoAssist_files
 set PATH=%PATH%;%LOCATION_ECOASSIST_FILES%
