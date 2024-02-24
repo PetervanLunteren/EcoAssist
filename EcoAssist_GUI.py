9fb24626289365fc2b65c343372682773399a9d9
Peter van Lunteren <contact@pvanlunteren.com>
2024-02-24 14:30:34 +0100

    added todo

--- EcoAssist_GUI.py
+++ EcoAssist_GUI.py
@@ -3,6 +3,7 @@
 # Created by Peter van Lunteren
 # Latest edit by Peter van Lunteren on 23 Feb 2024
 
+# TODO: WINDOW RESIZE - see email Saul. Fix window resize after switch_mode() and deploy_model()
 # TODO: DTYPES - specify dtypes for excel: https://stackoverflow.com/questions/24251219/pandas-read-csv-low-memory-and-dtype-options
 # TODO: EARLY EXIT - count the number of expected rows when exporting to excel: ValueError: This sheet is too large! Your sheet size is: 7152123, 1 Max sheet size is: 1048576, 16384
 # TODO: M2 - test on M2
