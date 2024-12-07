dec0767f443f4de6139c4561ac958c90b857e978
Peter van Lunteren <contact@pvanlunteren.com>
2024-12-07 12:57:32 +0100

    update MD version

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ echo off
 @setlocal EnableDelayedExpansion
 
 @REM log the install file version
-set DATE_OF_LAST_EDIT="27 Nov 2024"
+set DATE_OF_LAST_EDIT="7 Dec 2024"
 
 @REM installing version
 set CURRENT_VERSION=5.20
@@ -389,7 +389,7 @@ if exist "%LOCATION_ECOASSIST_FILES%\cameratraps\" (
         "%EA_GIT_EXE%" fetch --unshallow
     )
     cd "%LOCATION_ECOASSIST_FILES%\cameratraps" || ( echo "Could not change directory to cameratraps. Command could not be run. Installation was terminated. Copy-paste all text in this console window and send it to peter@addaxdatascience.com for further support." | wtee -a "%LOG_FILE%" & cmd /k & exit )
-    "%EA_GIT_EXE%" checkout a13bef87001cb4682330b954d33b3728f6e56fe7
+    "%EA_GIT_EXE%" checkout e8a4fc19a2b9ad1892dd9ce65d437252df271576
     cd "%LOCATION_ECOASSIST_FILES%" || ( echo "Could not change directory to EcoAssist_files. Command could not be run. Installation was terminated. Copy-paste all text in this console window and send it to peter@addaxdatascience.com for further support." | wtee -a "%LOG_FILE%" & cmd /k & exit )
     @REM check the size of the folder
     dir "%LOCATION_ECOASSIST_FILES%\cameratraps" | wtee -a "%LOG_FILE%"
