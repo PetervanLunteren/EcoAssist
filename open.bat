a7d502a71e89444555a3fbd7d13b9b2e08fd338a
Peter van Lunteren <contact@pvanlunteren.com>
2025-01-25 23:20:20 -0800

    add method to open in normal mode

--- open.bat
+++ open.bat
@@ -1,14 +1,22 @@
-@REM ### Placeholder script to open EcoAssist from inside Timelapse
-@REM ### This batch script is redundant since Timelapse can also open it by executing the python command directly,
-@REM ### but it is still here so that it mimics the old method. In the old method we needed this batch script to
-@REM ### perform some conda operations before opening the python script. With the Jan 2025 install update 
-@REM ### that is not neccesary anymore. But this script is still here so that Timelapse can run the same command as
-@REM ### before and users don't have to update their timelapse version when using a new EcoAssist version. 
-@REM ### Peter van Lunteren, 16 Jan 2025 (latest edit)
+@REM This script opens the EcoAssist program and allows for shortcut creation and execution from within Timelapse.
+@REM Timelapse can also open it by executing the python command directly, but it is still here so that it mimics 
+@REM the old method. In the old method we needed this batch script to perform some conda operations before opening
+@REM the python script. With the Jan 2025 install update that is not neccesary anymore. But this script is still 
+@REM here so that Timelapse can run the same command as before and users don't have to update their Timelapse 
+@REM version when using a new EcoAssist version. 
+@REM Peter van Lunteren, 25 Jan 2025 (latest edit)
 
 @REM set echo settings
 echo off
 @setlocal EnableDelayedExpansion
 
-@REM run script in timelapse mode
-"%homedrive%%homepath%\EcoAssist_files\envs\env-base\python.exe" "%homedrive%%homepath%\EcoAssist_files\EcoAssist\EcoAssist_GUI.py" --timelapse-path=%2
+@REM set paths
+set "ECOASSIST_GUI=%homedrive%%homepath%\EcoAssist_files\EcoAssist\EcoAssist_GUI.py"
+set "PYTHON_EXE=%homedrive%%homepath%\EcoAssist_files\envs\env-base\python.exe"
+
+@REM run script in either timelapse or normal mode
+if "%1" == "timelapse" (
+    %PYTHON_EXE% %ECOASSIST_GUI% --timelapse-path=%2
+) else (
+    %PYTHON_EXE% %ECOASSIST_GUI%
+)
